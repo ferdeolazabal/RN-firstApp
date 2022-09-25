f3733b9bba7596d32f88dc05cff253e97052e01d
ferdeolazabal <ferdeolazabal@gmail.com>
2022-09-25 14:56:58 -0300

    Create new component Fab
    - Refactor of ContadorScreen
    - A new component, Fav, was created so that it can be reused
    - Ternary was added depending on whether it is for android or ios

---
+++ src/components/Fab.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+interface Props {
+    title: string;
+    position?: 'br' | 'bl';
+    onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+    const ios = () => {
+        return (
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={[styles.fabLocation, position === 'bl' ? styles.left : styles.right]}
+                onPress={onPress}>
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>{title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    const android = () => {
+        return (
+            <View style={[styles.fabLocation, position === 'bl' ? styles.left : styles.right]}>
+                <TouchableNativeFeedback
+                    onPress={onPress}
+                    background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+                    <View style={styles.fab}>
+                        <Text style={styles.fabText}>{title}</Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>
+        );
+    };
+
+    return Platform.OS === 'ios' ? ios() : android();
+};
+
+const styles = StyleSheet.create({
+    fabLocation: {
+        position: 'absolute',
+        bottom: 25,
+    },
+    right: {
+        right: 25,
+    },
+    left: {
+        left: 25,
+    },
+    fab: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+        elevation: 2,
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
+});
